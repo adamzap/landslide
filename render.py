c4acf7f5d337cec91898638f3b33f62c335a725a
Adam Zapletal <adamzap@gmail.com>
2010-06-04 01:57:32 -0500

    Code highlighting works (one snippet per slide)

--- render.py
+++ render.py
@@ -1,5 +1,10 @@
+import re
 import jinja2
 import markdown
+import pygments
+
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
 
 with open('presentation.html', 'w') as outfile:
     slides_src = markdown.markdown(open('slides.md').read()).split('<hr />\n')
@@ -12,6 +17,26 @@ with open('presentation.html', 'w') as outfile:
 
     for slide_src in slides_src:
         header, content = slide_src.split('\n', 1)
+
+        lang_match = re.search('<code>!(.+)\n', content)
+
+        if lang_match:
+            lang = lang_match.group(1)
+            code = content.split(lang, 1)[1].split('</code')[0]
+
+            lexer = get_lexer_by_name(lang)
+
+            formatter = HtmlFormatter(linenos='inline', noclasses=True,
+                                      nobackground=True)
+
+            pretty_code = pygments.highlight(code, lexer, formatter)
+            pretty_code = pretty_code.replace('&amp;', '&')
+
+            before_code = content.split('<code>')[0]
+            after_code = content.split('</code>')[1]
+
+            content = before_code + pretty_code + after_code
+
         slides.append({'header': header, 'content': content})
 
     template = jinja2.Template(open('base.html').read())
