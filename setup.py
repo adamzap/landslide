b6d347a7a87b730517edf44123a1745cc6a10293
Stéphane Klein <stephane@harobed.org>
2010-07-24 20:07:54 +0200

    Distribute support and little refactoring
    * Append distribute (old setuptools) support
    * main module refactoring

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='landslide',
-    packages=['landslide'],
-    package_data={'landslide': [
-        'themes/default/*.html',
-        'themes/default/css/*.css',
-        'themes/default/js/*.js']},
     version='0.6.0',
     description='Lightweight markup language-based html5 slideshow generator',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
     author='Adam Zapletal',
     author_email='adamzap@gmail.com',
     url='http://github.com/adamzap/landslide',
@@ -21,8 +20,7 @@ setup(
         'rst',
         'restructuredtext'
     ],
-    requires=['Jinja2', 'Markdown', 'Pygments', 'docutils'],
-    scripts=['landslide/landslide'],
+    install_requires=['Jinja2', 'Markdown', 'Pygments', 'docutils'],
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.5',
@@ -39,4 +37,10 @@ Landslide takes your Markdown or RST file(s) and generates a slideshow like
 
 Read the `README <http://github.com/adamzap/landslide/blob/master/README.md>`_
 for formatting instructions and more information.
-""")
+""",
+    entry_points={
+        "console_scripts": [
+            "landslide = landslide.main:main",
+        ]
+    },
+)
