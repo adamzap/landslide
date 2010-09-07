e8683e37246663faecb5bc17c52a8ce235f2e513
Adam Zapletal <adamzap@gmail.com>
2010-09-06 23:23:45 -0500

    Bumped version number to 0.8.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='landslide',
-    version='0.6.0',
+    version='0.8.0',
     description='Lightweight markup language-based html5 slideshow generator',
     packages=find_packages('src'),
     package_dir = {'': 'src'},
