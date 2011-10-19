2489e90c1b2fc19b5bec9a1ff757a8cf082dcbf2
Adam Zapletal <adamzap@gmail.com>
2011-10-18 23:07:19 -0500

    Fix stdin/stdout typo and formatting in main

--- src/landslide/main.py
+++ src/landslide/main.py
@@ -47,7 +47,7 @@ def _parse_options():
         "-b", "--debug",
         action="store_true",
         dest="debug",
-        help="Will display any exception trace to stdin",
+        help="Will display any exception trace to stdout",
         default=False)
 
     parser.add_option(
@@ -90,7 +90,7 @@ def _parse_options():
         "-o", "--direct-output",
         action="store_true",
         dest="direct",
-        help="Prints the generated HTML code to stdin; won't work with PDF "
+        help="Prints the generated HTML code to stdout; won't work with PDF "
              "export",
         default=False)
 
@@ -98,7 +98,7 @@ def _parse_options():
         "-q", "--quiet",
         action="store_false",
         dest="verbose",
-        help="Won't write anything to stdin (silent mode)",
+        help="Won't write anything to stdout (silent mode)",
         default=False)
 
     parser.add_option(
@@ -121,8 +121,7 @@ def _parse_options():
         "-v", "--verbose",
         action="store_true",
         dest="verbose",
-        help="Write informational messages to stdin (enabled by "
-        "default)",
+        help="Write informational messages to stdout (enabled by default)",
         default=True)
 
     parser.add_option(
