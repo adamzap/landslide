11ceed5c885b3bf45d53d2bc2ff7642db8dbd73e
Adam Zapletal <adamzap@gmail.com>
2012-11-12 12:52:28 -0600

    Bump version to v1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='landslide',
-    version='1.1.0',
+    version='1.1.1',
     description='Lightweight markup language-based html5 slideshow generator',
     packages=find_packages('src'),
     package_dir = {'': 'src'},
