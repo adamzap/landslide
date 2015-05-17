f6aa75892d9095097496c5b344c8b2b5c330c023
Adam Zapletal <adamzap@gmail.com>
2015-05-17 15:58:53 -0500

    Finish Options class and related tests

--- landslide/options.py
+++ landslide/options.py
@@ -2,6 +2,19 @@ from six.moves import configparser
 
 
 class Options(object):
+    theme = 'default'
+    linenos = 'inline'
+    encoding = 'utf8'
+    destination = 'presentation.html'
+
+    js = css = extensions = []
+
+    def __getattribute__(self, name):
+        try:
+            return object.__getattribute__(self, name)
+        except AttributeError:
+            return False
+
     def __init__(self, args):
         self.has_config_file = args['<source-or-config-file>'].endswith('.cfg')
 
@@ -9,6 +22,15 @@ class Options(object):
             self.parse_config_file(args['<source-or-config-file>'])
 
         self.parse_arguments(args)
+
+        if self.js:
+            self.js = self.js.split('\n')
+
+        if self.css:
+            self.css = self.css.split('\n')
+
+        if self.extensions:
+            self.extensions = self.extensions.split(',')
 
     def parse_config_file(self, config_file):
         parser = configparser.RawConfigParser()
@@ -19,16 +41,26 @@ class Options(object):
 
         self.sources = [s.strip() for s in config.get('source').split('\n')]
 
-        self.theme = config.get('theme')
-        self.destination = config.get('destination')
-        self.linenos = config.get('linenos')
-        self.extensions = config.get('extensions')
-        self.css = config.get('css').split('\n')
-        self.js = config.get('js').split('\n')
+        self.theme = config.get('theme', self.theme)
+        self.linenos = config.get('linenos', self.linenos)
+        self.destination = config.get('destination', self.destination)
+        self.encoding = config.get('encoding', self.encoding)
+        self.extensions = config.get('extensions', self.extensions)
+        self.css = config.get('css', self.css)
+        self.js = config.get('js', self.js)
+        self.copy_theme = config.get('copy_theme')
+        self.debug = config.get('debug')
+        self.embed = config.get('embed')
+        self.direct_output = config.get('direct_output')
+        self.no_presenter_notes = config.get('no_presenter_notes')
+        self.quiet = config.get('quiet')
+        self.relative = config.get('relative')
+        self.watch = config.get('watch')
+        self.math_output = config.get('math_output')
 
     def parse_arguments(self, args):
         if not self.has_config_file:
-            self.sources.insert(0, args['<source-or-config-file>'])
+            self.sources = [args['<source-or-config-file>']]
 
         self.sources.extend(args['<source-files>'])
 
---
+++ tests.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+import unittest
+
+from docopt import docopt
+
+from landslide import cli
+from landslide.options import Options
+
+
+class OptionsTestCase(unittest.TestCase):
+    def call(self, s):
+        return Options(docopt(cli.__doc__, s.split(' ')))
+
+    def get_option(self, switch, name):
+        return getattr(self.call('in.md %s' % switch), name)
+
+    def test_single_source(self):
+        options = self.call('in.md')
+
+        self.assertEqual(options.sources, ['in.md'])
+
+    def test_multiple_sources(self):
+        options = self.call('a.md b.md c.md d.md')
+
+        self.assertEqual(options.sources, ['a.md', 'b.md', 'c.md', 'd.md'])
+
+    def test_default_options(self):
+        options = self.call('in.md')
+
+        self.assertEqual(options.theme, 'default')
+        self.assertEqual(options.linenos, 'inline')
+        self.assertEqual(options.destination, 'presentation.html')
+        self.assertEqual(options.encoding, 'utf8')
+        self.assertEqual(options.extensions, [])
+        self.assertEqual(options.css, [])
+        self.assertEqual(options.js, [])
+        self.assertFalse(options.copy_theme)
+        self.assertFalse(options.debug)
+        self.assertFalse(options.embed)
+        self.assertFalse(options.direct_output)
+        self.assertFalse(options.no_presenter_notes)
+        self.assertFalse(options.quiet)
+        self.assertFalse(options.relative)
+        self.assertFalse(options.watch)
+        self.assertFalse(options.math_output)
+
+    def test_all_options_config_file(self):
+        options = self.call('test-data/all-options.cfg')
+
+        self.assertEqual(options.sources, ['a.md', 'b-dir'])
+        self.assertEqual(options.theme, '/path/to/theme')
+        self.assertEqual(options.linenos, 'no')
+        self.assertEqual(options.destination, 'out.html')
+        self.assertEqual(options.encoding, 'utf16')
+        self.assertEqual(options.extensions, ['a', 'b'])
+        self.assertEqual(options.css, ['style-1.css', 'style-2.css'])
+        self.assertEqual(options.js, ['js-1.js', 'js-2.js'])
+        self.assertTrue(options.copy_theme)
+        self.assertTrue(options.debug)
+        self.assertTrue(options.embed)
+        self.assertTrue(options.direct_output)
+        self.assertTrue(options.no_presenter_notes)
+        self.assertTrue(options.quiet)
+        self.assertTrue(options.relative)
+        self.assertTrue(options.watch)
+        self.assertTrue(options.math_output)
+
+    def test_source_only_config_file(self):
+        options = self.call('test-data/sources-only.cfg')
+
+        self.assertEqual(options.theme, 'default')
+        self.assertEqual(options.linenos, 'inline')
+        self.assertEqual(options.destination, 'presentation.html')
+        self.assertEqual(options.encoding, 'utf8')
+        self.assertEqual(options.extensions, [])
+        self.assertEqual(options.css, [])
+        self.assertEqual(options.js, [])
+        self.assertFalse(options.copy_theme)
+        self.assertFalse(options.debug)
+        self.assertFalse(options.embed)
+        self.assertFalse(options.direct_output)
+        self.assertFalse(options.no_presenter_notes)
+        self.assertFalse(options.quiet)
+        self.assertFalse(options.relative)
+        self.assertFalse(options.watch)
+        self.assertFalse(options.math_output)
+
+    def test_config_file_with_multiple_sources(self):
+        options = self.call('test-data/sources-only.cfg c.md')
+
+        self.assertEqual(options.sources, ['a.md', 'b-dir', 'c.md'])
+
+    def test_theme_cli_option(self):
+        self.assertEqual(self.get_option('-t blue', 'theme'), 'blue')
+        self.assertEqual(self.get_option('--theme blue', 'theme'), 'blue')
+
+    def test_linenos_cli_option(self):
+        self.assertEqual(self.get_option('-l table', 'linenos'), 'table')
+        self.assertEqual(self.get_option('--linenos no', 'linenos'), 'no')
+
+    def test_destination_cli_option(self):
+        name = 'destination'
+
+        self.assertEqual(self.get_option('-d out', name), 'out')
+        self.assertEqual(self.get_option('--destination out', name), 'out')
+
+    def test_encoding_cli_option(self):
+        name = 'encoding'
+
+        self.assertEqual(self.get_option('-e utf16', name), 'utf16')
+        self.assertEqual(self.get_option('--encoding utf16', name), 'utf16')
+
+    def test_extensions_cli_option(self):
+        name = 'extensions'
+
+        self.assertTrue(self.get_option('-x a,b', name), ['a', 'b'])
+        self.assertTrue(self.get_option('--extensions a,b', name), ['a', 'b'])
+
+    def test_copy_theme_cli_option(self):
+        self.assertTrue(self.get_option('-c', 'copy_theme'))
+        self.assertTrue(self.get_option('--copy-theme', 'copy_theme'))
+
+    def test_debug_cli_option(self):
+        self.assertTrue(self.get_option('-b', 'debug'))
+        self.assertTrue(self.get_option('--debug', 'debug'))
+
+    def test_embed_cli_option(self):
+        self.assertTrue(self.get_option('-i', 'embed'))
+        self.assertTrue(self.get_option('--embed', 'embed'))
+
+    def test_direct_output_cli_option(self):
+        self.assertTrue(self.get_option('-o', 'direct_output'))
+        self.assertTrue(self.get_option('--direct-output', 'direct_output'))
+
+    def test_no_presenter_notes_cli_options(self):
+        name = 'no_presenter_notes'
+
+        self.assertTrue(self.get_option('-P', name))
+        self.assertTrue(self.get_option('--no-presenter-notes', name))
+
+    def test_quiet_cli_option(self):
+        self.assertTrue(self.get_option('-q', 'quiet'))
+        self.assertTrue(self.get_option('--quiet', 'quiet'))
+
+    def test_relative_cli_option(self):
+        self.assertTrue(self.get_option('-r', 'relative'))
+        self.assertTrue(self.get_option('--relative', 'relative'))
+
+    def test_watch_cli_option(self):
+        self.assertTrue(self.get_option('-w', 'watch'))
+        self.assertTrue(self.get_option('--watch', 'watch'))
+
+    def test_match_output_cli_option(self):
+        self.assertTrue(self.get_option('-m', 'math_output'))
+        self.assertTrue(self.get_option('--math-output', 'math_output'))
+
+
+if __name__ == '__main__':
+    unittest.main()
